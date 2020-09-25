f9818c300b907ac667b0740e9a2d8b551518c588
ateebasif902 <ateeb.asif902@gmail.com>
2020-09-26 01:27:28 +0500

    added validations in the contactAdd Form

--- src/components/Form/AddContactForm.jsx
+++ src/components/Form/AddContactForm.jsx
@@ -4,13 +4,50 @@ import { Field, reduxForm } from 'redux-form';
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+const nameValidateExp = /^[a-zA-Z ]{4}\w*$/; // [a-zA-Z ]{4}\w*$ it can accept digits also
+const numValidateExp = /^[3][0][1]\s\d{3}\s\d{4}$/;
+
 const validate = (values) => {
   const errors = {};
-  if (!values.username) {
+  //   Name Validation
+  if (!values.name) {
     errors.username = 'Required';
-  } else if (values.username.length > 15) {
-    errors.username = 'Must be 15 characters or less';
+  } else if (!nameValidateExp.test(values.name)) {
+    errors.name =
+      'Must be more than 3 characters and cannot contain any special character';
+  } else {
+    errors.name = 'Name is correct';
   }
+  // Phone Number Validation
+  if (!values.phoneNumber) {
+    errors.phoneNumber = 'Required';
+  } else if (numValidateExp.test(values.phoneNumber)) {
+    errors.phoneNumber = 'Phone is correct';
+  } else {
+    errors.phoneNumber =
+      'Phone is incorrect!, Format (301) *** ****, Must Start with 3 and then have 7 digits, it cannot contain alphabets special characters!';
+  }
+  //    Address Validation
+  if (!values.address) {
+    errors.address = 'Required';
+  }
+  //   else if (values.name.length <= 3 || values.name !== /^[a-z]/) {
+  else if (values.address.length > 3 && values.address.length < 200) {
+    errors.address = 'valid Address';
+  } else {
+    errors.address =
+      'Address must be greater than 3 characters and less than 200';
+  }
+  //   Bio Validation
+  if (!values.bio) {
+    errors.bio = 'Required';
+  } else if (values.bio.length > 10 && values.bio.length < 1000) {
+    errors.bio = 'Valid';
+  } else {
+    errors.bio =
+      'Bio Must be more than 10 characters and less than 1000 character';
+  }
+
   if (!values.email) {
     errors.email = 'Required';
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
@@ -34,11 +71,17 @@ const warn = (values) => {
   return warnings;
 };
 
-const renderField = ({ input, label, type, meta: { touched, error } }) => (
+const renderField = ({
+  input,
+  label,
+  placeholder,
+  type,
+  meta: { touched, error },
+}) => (
   <div>
     <label>{label}</label>
     <div>
-      <input {...input} placeholder={label} type={type} />
+      <input {...input} placeholder={placeholder} type={type} />
       {touched && error && <span>{error}</span>}
     </div>
   </div>
@@ -62,23 +105,37 @@ const SubmitValidationForm = (props) => {
   };
   return (
     <form onSubmit={handleSubmit(submit)}>
-      <Field name="name" type="text" component={renderField} label="Name" />
+      <Field
+        name="name"
+        type="text"
+        component={renderField}
+        placeholder="Name"
+        label="Name"
+      />
       <Field
         name="address"
         type="text"
         component={renderField}
+        placeholder="Address"
         label="Address"
       />
-      <Field name="bio" type="text" component={renderField} label="Bio" />
+      <Field
+        name="bio"
+        type="text"
+        component={renderField}
+        placeholder="Bio"
+        label="Bio"
+      />
       <Field
         name="phoneNumber"
         type="text"
         component={renderField}
         label="PhoneNumber"
+        placeholder="Format (301) *** ****"
       />
       <Field
         name="profilePicture"
-        type="text"
+        type="file"
         component={renderField}
         label="ProfilePicture"
       />
